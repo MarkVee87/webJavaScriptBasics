eda2fe7da7795a2f9d5e17c4e4faefc8b7ef4e87
Mark Vallis <1034528@my.brunel.ac.uk>
2017-03-15 23:03:43 +0000

    getting hold of dom elements

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
-  var elem = document.getElementById("mydiv");
-  // document.getElementsByClassName(name)
+  var domElem = document.getElementById("mydiv");
+  domElem.innerHTML = "hello world";
   // document.getElementsByTagName(name)
-  elem.innerHTML = "hello world";
+  var arr = document.getElementsByClassName("divvy");
+    for (var x = 0; x < arr.length; x++) {
+      arr[x].innerHTML = "Hi there";
+    }
+  var arr1 = document.getElementsByClassName("divver");
+    for (var x = 0; x < arr1.length; x++) {
+      arr1[x].innerHTML = "Hellooooooooooooooo";
+    }
 });
