2359950e6b17815102bcf11867da6f0461d41cdb
Mark Vallis <1034528@my.brunel.ac.uk>
2017-03-15 13:23:04 +0000

    three different array types used

--- js/moreobjects.js
+++ js/moreobjects.js
@@ -1,3 +1,16 @@
+// static array
 var fruits = new Array("apples", "oranges", "pears");
+document.write(fruits + "<br />");
 
-document.write(fruits);
+// fixed size/length array
+var vegetables = new Array(3);
+vegetables[0] = "potato";
+vegetables[1] = "turnip";
+vegetables[2] = "donut";
+document.write(vegetables + "<br />");
+
+//dynamic array
+var vehicles = new Array();
+vehicles[0] = "car";
+vehicles[1] = "unicycle";
+document.write(vehicles + "<br />");
