701223bf5bbcbef3c99cd5c927316b00ac2e28c2
Mark Vallis <1034528@my.brunel.ac.uk>
2017-03-15 23:21:50 +0000

    using childnodes of a div

--- js/dom.js
+++ js/dom.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
   var domElem = document.getElementById("mydiv");
   domElem.innerHTML = "hello world";
-  // document.getElementsByTagName(name)
+  // document.getElementsByTagName("p");
+  // changing all elements with a div class name of 'divvy'
   var arr = document.getElementsByClassName("divvy");
-    for (var x = 0; x < arr.length; x++) {
-      arr[x].innerHTML = "Hi there";
-    }
+  for (var x = 0; x < arr.length; x++) {
+    arr[x].innerHTML = "Hi there";
+  }
+  // changing all elements with a div class name of 'divver'
   var arr1 = document.getElementsByClassName("divver");
-    for (var x = 0; x < arr1.length; x++) {
-      arr1[x].innerHTML = "Hellooooooooooooooo";
-    }
+  for (var x = 0; x < arr1.length; x++) {
+    arr1[x].innerHTML = "Hellooooooooooooooo";
+  }
+
+  var arr2 = document.getElementById("anotherDiv");
+  var a = arr2.childNodes;
+  for(var x = 0; x < a.length; x++) {
+    a[x].innerHTML = "look, some new text";
+  }
 });
