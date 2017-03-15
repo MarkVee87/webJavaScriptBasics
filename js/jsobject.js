0d2ca84054b36c8163125ddbc161a23ac2aa6065
Mark Vallis <1034528@my.brunel.ac.uk>
2017-03-15 12:56:57 +0000

    playing with objects some more

--- js/jsobject.js
+++ js/jsobject.js
@@ -5,9 +5,11 @@ function person(n, a){
 
 var aName = prompt("please enter your name");
 var aAge = prompt("please enter your age");
+alert('thanks :)');
 
 var a = new person(aName, aAge);
 var b = new person("bob", 21);
+var aSingleInstanceOfAnObject = {faveFood: "steak", fingers: 10};
 
 document.write(a.name + " " + a.age + "<br />");
-document.write(b.name + " " + b.age);
+document.write(b.name + " " + b.age + " " + aSingleInstanceOfAnObject.faveFood);
