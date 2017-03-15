9cac07fc9bdcd0e1c938f411fab6f84e126ad06b
Mark Vallis <1034528@my.brunel.ac.uk>
2017-03-15 13:01:25 +0000

    using methods in objects

--- js/jsobject.js
+++ js/jsobject.js
@@ -1,6 +1,10 @@
 function person(n, a){
   this.name = n;
   this.age = a;
+  // Method = function called as a parameter of an object
+  this.changeName = function(name){
+    this.name = name;
+  }
 }
 
 var aName = prompt("please enter your name");
@@ -12,4 +16,7 @@ var b = new person("bob", 21);
 var aSingleInstanceOfAnObject = {faveFood: "steak", fingers: 10};
 
 document.write(a.name + " " + a.age + "<br />");
-document.write(b.name + " " + b.age + " " + aSingleInstanceOfAnObject.faveFood);
+document.write(b.name + " " + b.age + " " + aSingleInstanceOfAnObject.faveFood  + "<br />");
+// Calling the method of the person object 'a'
+a.changeName("dave the goat");
+document.write(a.name + " " + a.age + "<br />");
